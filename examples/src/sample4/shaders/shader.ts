2639af70c88ac0935df0e2868b12a8dba92667d9
feng <908087098@qq.com>
2025-11-24 01:20:12 +0800

    refactor: 更新着色器结构体并调整变量处理逻辑
    - 引入 Varying 结构体，替换 VertexOutput 结构体中的位置字段
    - 修改顶点着色器和片段着色器中的变量声明，确保正确处理输出
    - 注释掉原有的 VertexOutput 结构体定义，提升代码可读性

--- examples/src/sample4/shaders/shader.ts
+++ examples/src/sample4/shaders/shader.ts
@@ -8,18 +8,43 @@ const aVertexColor = vec4(attribute("aVertexColor", 1));
 const uModelViewMatrix = mat4(uniform("uModelViewMatrix", 0, 0));
 const uProjectionMatrix = mat4(uniform("uProjectionMatrix", 1, 0));
 
-const VertexOutput = struct("VertexOutput", {
-    position: vec4(builtin("position", "position_vec4")),
+// const VertexOutput = struct("VertexOutput", {
+//     position: vec4(builtin("position", "position_vec4")),
+//     color: vec4(varying("vColor", 0)),
+// });
+
+// // Vertex shader 入口函数
+// export const vertexShader = vertex("main", () =>
+// {
+//     const output = var_('output', VertexOutput);
+//     const position = var_("position", vec4(aVertexPosition, 0.0, 1.0));
+
+//     assign(output.position, uProjectionMatrix.multiply(uModelViewMatrix).multiply(position));
+//     assign(output.color, aVertexColor);
+//     return_(output as any);
+// });
+
+// // Fragment shader 入口函数
+// export const fragmentShader = fragment("main", () =>
+// {
+//     precision('lowp');
+//     const input = var_('input', VertexOutput);
+
+//     return_(input.color);
+// });
+
+const Varying = struct("Varying", {
+    position1: vec4(builtin("position", "position_vec4")),
     color: vec4(varying("vColor", 0)),
 });
 
 // Vertex shader 入口函数
 export const vertexShader = vertex("main", () =>
 {
-    const output = var_('output', VertexOutput);
+    const output = var_('v', Varying);
     const position = var_("position", vec4(aVertexPosition, 0.0, 1.0));
 
-    assign(output.position, uProjectionMatrix.multiply(uModelViewMatrix).multiply(position));
+    assign(output.position1, uProjectionMatrix.multiply(uModelViewMatrix).multiply(position));
     assign(output.color, aVertexColor);
     return_(output as any);
 });
@@ -28,7 +53,7 @@ export const vertexShader = vertex("main", () =>
 export const fragmentShader = fragment("main", () =>
 {
     precision('lowp');
-    const input = var_('input', VertexOutput);
+    const input = var_('v', Varying);
 
     return_(input.color);
 });
